59836632fd6daa7c8f9b3e201dd1b5b033afd78c
Shathwik Reddy Duvvuru <dshathwikr@gmail.com>
2025-06-01 18:05:52 +0530

    Added auto redirect to dashboard if signed in

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,16 @@
 import { Link } from "react-router-dom";
 import "./Home.css";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 function Home() {
+  const navigate = useNavigate();
+  const token = localStorage.getItem("token");
+  
+  useEffect(()=>{
+    if (token) navigate("/dashboard")
+  }, [token, navigate])
+
   return (
     <div className="home-container">
       <h1 className="title">Welcome to the Banking System</h1>
